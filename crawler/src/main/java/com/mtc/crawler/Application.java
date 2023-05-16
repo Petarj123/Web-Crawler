f5960fa61a05fce3d8fd0b67ad4a93f27fcf08ae
Petarj123 <pjankovic03@gmail.com>
2023-05-16 17:51:37 +0200

    Cleaning up

--- crawler/src/main/java/com/mtc/crawler/Application.java
+++ crawler/src/main/java/com/mtc/crawler/Application.java
@@ -1,5 +1,6 @@
 package com.mtc.crawler;
 
+import com.mtc.crawler.service.PdfScraper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		PdfScraper scraper = new PdfScraper();
+		scraper.extractPdf("https://www.africau.edu/images/default/sample.pdf");
 	}
 
 }
---
+++ crawler/src/main/java/com/mtc/crawler/service/PdfScraper.java
@@ -0,0 +1,9 @@
+package com.mtc.crawler.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PdfScraper {
+
+
+}
